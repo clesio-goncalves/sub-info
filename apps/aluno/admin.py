418193b22bfab7e6d13b9f2a82bf55a4705d08dc
clesio-goncalves <clesio.pi@hotmail.com>
2025-03-17 21:14:34 -0300

    Pagina de cadastro

--- apps/aluno/admin.py
+++ apps/aluno/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from apps.aluno.models import Aluno
 
-admin.site.register(Aluno)
+class ListandoAlunos(admin.ModelAdmin):
+    list_display = ("id", "matricula", "nome", "telefone", "status")
+    list_display_links = ("id", "matricula")
+    search_fields = ("matricula", "nome")
+    list_filter = ("status",) # adicionado
+    list_per_page = 10
+
+admin.site.register(Aluno, ListandoAlunos)
---
+++ apps/aluno/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from apps.aluno.models import Aluno
+
+
+class AlunoForms(forms.ModelForm):
+    class Meta:
+        model = Aluno
+        exclude = ["",]
+        labels = {
+            "matricula": "Matrícula",
+            "status": "Ativo",
+        }
+
+        widgets = {
+            "nome": forms.TextInput(attrs={"class": "form-control", "autofocus":"autofocus"}),
+            "matricula": forms.TextInput(attrs={"class": "form-control"}),
+            "telefone": forms.TextInput(attrs={"class": "form-control"}),
+            "email": forms.EmailInput(attrs={"class": "form-control"}),
+            "status": forms.CheckboxInput(attrs={"class": "form-control form-check-input"}),
+        }
