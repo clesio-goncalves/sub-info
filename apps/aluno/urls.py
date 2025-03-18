766f1ebf13a5352aedd579d2c15695ab82abaa77
clesio-goncalves <clesio.pi@hotmail.com>
2025-03-18 20:26:50 -0300

    Pagina de cadastro

--- apps/aluno/urls.py
+++ apps/aluno/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from apps.aluno.views import index, listar, cadastrar
+from apps.aluno.views import index, listar, cadastrar, exibir, editar
 
 urlpatterns = [
     path("", index, name="index"),
     path("lista", listar, name="aluno/lista"),
-    path("cadastro", cadastrar, name="aluno/cadastro")
+    path("cadastro", cadastrar, name="aluno/cadastro"),
+    path("exibe", exibir, name="aluno/exibe"),
+    path("edita", editar, name="aluno/edita")
 ]
--- apps/aluno/views.py
+++ apps/aluno/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from apps.aluno.models import Aluno
 from apps.aluno.forms import AlunoForms
 
@@ -6,11 +6,23 @@ def index(request):
     return render(request, 'aluno/index.html')
 
 def listar(request):
-    alunos = Aluno.objects.order_by("nome")
+    alunos = Aluno.objects.order_by("-id")
 
     return render(request, "aluno/lista.html", {"alunos": alunos})
 
 def cadastrar(request):
     form = AlunoForms()
+
+    if request.method == 'POST':
+        form = AlunoForms(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect("aluno/lista")
     
     return render(request, "aluno/cadastro.html", {"form": form})
+
+def editar(request):
+    pass
+
+def exibir(request):
+    pass
